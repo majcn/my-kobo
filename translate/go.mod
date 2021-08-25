d5c024d88cfc8e10f17c05b8b9001f4e7b14dcdf
Gregor Majcen <gregor.majcen@sinergise.com>
2021-08-25 11:58:32 +0200

    translate

---
+++ translate/go.mod
@@ -0,0 +1,9 @@
+module translate
+
+go 1.17
+
+require (
+	github.com/tidwall/gjson v1.8.1
+	github.com/tidwall/match v1.0.3 // indirect
+	github.com/tidwall/pretty v1.1.0 // indirect
+)
---
+++ translate/program.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	fullTranslate "translate/googletranslate"
+	simpleTranslate "translate/googletranslatefree"
+)
+
+func printTranslate(result interface{}, err error) {
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
+
+func main() {
+	// curl -L -o ca-bundle.crt https://curl.haxx.se/ca/cacert.pem
+	os.Setenv("SSL_CERT_FILE", "/mnt/onboard/.adds/certs/ca-bundle.crt")
+
+	text := os.Args[1]
+	isSimple := os.Args[2] == "S"
+
+	if isSimple {
+		printTranslate(simpleTranslate.Translate(text, "en", "sl"))
+	} else {
+		printTranslate(fullTranslate.Translate(text, "en", "sl"))
+	}
+}
